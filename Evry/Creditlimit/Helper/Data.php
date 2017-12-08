3566d843ddbacd47defb7293f16d200341e5037a
Nithin Havaldar <nitinhavaldar@yahoo.com>
2017-12-08 12:50:43 +0530

    Added baseUrl in the helper

--- Evry/Creditlimit/Helper/Data.php
+++ Evry/Creditlimit/Helper/Data.php
@@ -7,21 +7,28 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper {
 
    protected $scopeConfig;
    protected $_date;
+   protected $_storeManager;
 
    const XML_PATH_MODULE_ENABLE = 'credit_limit/creditlimit_settings/enable';
    const XML_PATH_CREDIT_AMOUNT = 'credit_limit/creditlimit_settings/limit';  
 
-   public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,DateTime $date)
+   public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+							   \Magento\Store\Model\StoreManagerInterface $storeManager,
+								DateTime $date)
    {
       $this->scopeConfig = $scopeConfig;
+	   $this->_storeManager = $storeManager;
       $this->_date = $date;
    }
+   
+   public function getBaseUrl() {
+        return $this->_storeManager->getStore()->getBaseUrl();
+    }
 
    public function isEnabled() {
         return $this->scopeConfig
                 ->getValue(self::XML_PATH_MODULE_ENABLE, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
-
 
    public function getCreditLimit() {
         return $this->scopeConfig
